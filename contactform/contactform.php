dade92b2ad3826a5efce446a5e815b2f381f3398
Tavleen Singh <tavleensingh26@gmail.com>
2020-04-22 15:27:25 -0400

    inital commit

---
+++ contactform/contactform.php
@@ -0,0 +1,18 @@
+<?php
+$allowedOrigins = array(
+  '(http(s)://)?(www\.)?my\-domain\.com'
+);
+ 
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+  foreach ($allowedOrigins as $allowedOrigin) {
+    if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+      header('Access-Control-Allow-Origin:*');
+      header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+      header('Access-Control-Max-Age: 1000');
+      header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+      break;
+    }
+  }
+}
+echo "OK";
+?>
